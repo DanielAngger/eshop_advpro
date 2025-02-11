437fae71f57c2d9cc6800dc5d3ef61a39a27ab59
DanielAngger <angger.7d@gmail.com>
2025-02-11 20:45:51 +0700

    Fuctional Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
+val junitJupiterVersion = "5.10.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
@@ -37,10 +37,9 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
-	testImplementation("jo.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
-	testImplementation("jo.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-	testImplementation ("org.junit. jupiter:junit-jupiter-api:$junitJupiterVersion")
-	testRuntimeOnly("org.junit. jupiter: junit-jupiter-engine:$junitJupiterVersion")
+	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
