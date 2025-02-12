d169450e2467dc23854a2549a4d99261bfa40bb1
DanielAngger <angger.7d@gmail.com>
2025-02-12 14:40:32 +0700

    Unit test

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/TestDeleteProduct.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/TestDeleteProduct.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith (MockitoExtension.class)
@@ -28,5 +30,29 @@ public class TestDeleteProduct {
         product.setProductQuantity(100);
         productRepository.delete(product.getProductId());
         assertFalse(productRepository.findAll().hasNext());
+    }
+
+    @Test
+    void testDeletedProductNotFound() {
+        product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        productRepository.delete(product.getProductId());
+
+        boolean found = false;
+        Iterator<Product> products = productRepository.findAll();
+
+        while (products.hasNext()) {
+            Product p = products.next();
+            if (p.getProductId().equals("eb558e9f-1c39-460e-8860-71af6af63bd6")) {
+                found = true;
+                break;
+            }
+        }
+
+        assertFalse(found, "Deleted product should not be found in the repository");
     }
 }
