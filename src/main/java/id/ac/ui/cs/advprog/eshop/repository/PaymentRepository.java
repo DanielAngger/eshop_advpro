e78a6d53c9409d9c3e6bb3269bfccebf23ad9596
DanielAngger <angger.7d@gmail.com>
2025-03-04 18:36:53 +0700

    [GREEN] Implement Payment reporitory

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import java.util.*;
+
+public class PaymentRepository {
+    private final Map<String, Payment> paymentStorage = new HashMap<>();
+
+    public void save(Payment payment) {
+        paymentStorage.put(payment.getId(), payment);
+    }
+
+    public Optional<Payment> findById(String id) {
+        return Optional.ofNullable(paymentStorage.get(id));
+    }
+
+    public void delete(String id) {
+        paymentStorage.remove(id);
+    }
+
+    public List<Payment> findAll() {
+        return new ArrayList<>(paymentStorage.values());
+    }
+}
